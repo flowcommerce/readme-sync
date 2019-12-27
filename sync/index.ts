ed076cdd97ee4fa89d6f5987cf3315418c693b5f
Ben Iofel <ben@flow.io>
2019-12-26 19:15:14 -0500

    Fix sync logic and add colors

--- sync/index.ts
+++ sync/index.ts
@@ -2,12 +2,13 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import * as yargs from 'yargs'
-import { slugify } from './slugify'
 import { Client } from './client'
 import { HTTPError } from 'got'
 import * as matter from 'gray-matter'
 import * as assert from 'assert'
 import { DocForm, Category, DocSummaryParent, Doc } from './generated/readme'
+import { slugify } from './slugify'
+import { blueBright, green, yellow, redBright } from 'chalk'
 
 const argv = yargs
     .version(false)
@@ -22,11 +23,11 @@ const client = new Client(argv.apiKey, argv.version)
 type RemoteTreeEntry = { category: Category; docs: DocSummaryParent[] }
 type RemoteTree = Map<string, RemoteTreeEntry>
 
-function upsertDoc(remoteTree: RemoteTree, category: string, filepath: string, options: { parentDoc?: string; slug?: string } = {}): Promise<Doc> {
+function upsertDoc(remoteTree: RemoteTree, categoryName: string, filepath: string, options: { parent?: Doc; slug?: string } = {}): Promise<Doc> {
     assert(fs.statSync(filepath).isFile)
-    const slug = options.slug || slugify(path.parse(filepath).name)
+    const slug = options.slug || path.parse(filepath).name
 
-    const existing = remoteTree.get(slugify(category)).docs.find((doc) => {
+    const existing = remoteTree.get(slugify(categoryName)).docs.find((doc) => {
         if (doc.slug === slug)
             return true
 
@@ -39,23 +40,24 @@ function upsertDoc(remoteTree: RemoteTree, category: string, filepath: string, o
         slug,
         title: metadata.data.title,
         body: metadata.content,
-        category: remoteTree.get(slugify(category)).category._id,
-        parentDoc: options.parentDoc,
+        category: remoteTree.get(slugify(categoryName)).category._id,
+        parentDoc: options.parent ? options.parent._id : undefined,
         hidden: false,
     }
 
+    const destination = `${categoryName}${options.parent ? ` / ${options.parent.title}` : ''} / ${metadata.data.title}`
+
     if (existing) {
-        console.log(`\tUpdating ${filepath} -> ${category} / ${slug}`)
+        console.log(`\tUpdating ${blueBright(filepath)} -> ${green(destination)}`)
         return client.updateDoc(slug, form).then(x => x.body)
     } else {
-        console.log(`\tCreating ${filepath} -> ${category} / ${slug}`)
+        console.log(`\tCreating ${blueBright(filepath)} -> ${green(destination)}`)
         return client.createDoc(form).then(x => x.body)
     }
 }
 
-async function upsertDir(remoteTree: RemoteTree, category: string, dirpath: string): Promise<void> {
+async function upsertDir(remoteTree: RemoteTree, categoryName: string, dirpath: string): Promise<void> {
     assert(fs.statSync(dirpath).isDirectory)
-    console.log(`\tSyncing dir ${dirpath}`)
 
     const children = fs.readdirSync(dirpath)
     if (!children.includes('index.md')) {
@@ -63,30 +65,32 @@ async function upsertDir(remoteTree: RemoteTree, category: string, dirpath: stri
         return
     }
 
-    const parent = await upsertDoc(remoteTree, category, path.join(dirpath, 'index.md'), { slug: slugify(path.basename(dirpath)) })
+    const parent = await upsertDoc(remoteTree, categoryName, path.join(dirpath, 'index.md'), { slug: slugify(path.basename(dirpath)) })
 
     for (const child of children) {
         if (child === 'index.md')
             continue
 
-        await upsertDoc(remoteTree, category, path.join(dirpath, child), { parentDoc: parent._id })
+        await upsertDoc(remoteTree, categoryName, path.join(dirpath, child), { parent })
     }
 }
 
-async function deleteNotPresent({ category, docs }: RemoteTreeEntry): Promise<void> {
+async function deleteNotPresent({ category, docs }: RemoteTreeEntry, categoryDir: string): Promise<void> {
     for (const doc of docs) {
+        const dir = fs.readdirSync(categoryDir).find(d => slugify(d) === doc.slug)
+
         // delete children
         for (const child of doc.children) {
-            if (!fs.existsSync(path.join(argv.docs, category.slug, doc.slug, child.slug + '.md'))) {
-                console.log(`\tDeleting remote ${category.slug} / ${doc.slug} / ${child.slug}`)
-                await client.deleteDoc(doc.slug)
+            if (!dir || !fs.existsSync(path.join(categoryDir, dir, child.slug + '.md'))) {
+                console.log(`\tDeleting remote ${redBright(`${category.title} / ${doc.title} / ${child.title}`)}`)
+                await client.deleteDoc(child.slug)
             }
         }
 
         // delete parents
-        if (!fs.existsSync(path.join(argv.docs, category.slug, doc.slug, 'index.md'))
-         && !fs.existsSync(path.join(argv.docs, category.slug, doc.slug + '.md'))) {
-            console.log(`\tDeleting remote ${category.slug} / ${doc.slug}`)
+        if (!(dir && fs.existsSync(path.join(categoryDir, dir, 'index.md')))
+         && !fs.existsSync(path.join(categoryDir, doc.slug + '.md'))) {
+            console.log(`\tDeleting remote ${redBright(`${category.title} / ${doc.title}`)}`)
             await client.deleteDoc(doc.slug)
         }
     }
@@ -104,18 +108,23 @@ async function deleteNotPresent({ category, docs }: RemoteTreeEntry): Promise<vo
  */
 async function sync(remoteTree: RemoteTree): Promise<void> {
     for (const category of fs.readdirSync(argv.docs)) {
+        if (category.startsWith('.'))
+            continue
+
         console.log(category)
         const categoryPath = path.join(argv.docs, category)
         for (const doc of fs.readdirSync(categoryPath)) {
             const docPath = path.join(categoryPath, doc)
-            if (doc.endsWith('.md')) {
+            if (doc.startsWith('.')) {
+                continue
+            } else if (doc.endsWith('.md')) {
                 await upsertDoc(remoteTree, category, docPath)
             } else {
                 await upsertDir(remoteTree, category, path.join(argv.docs, category, doc))
             }
         }
 
-        await deleteNotPresent(remoteTree.get(slugify(category)))
+        await deleteNotPresent(remoteTree.get(slugify(category)), path.join(argv.docs, category))
     }
 }
 
@@ -126,13 +135,26 @@ async function main(): Promise<void> {
 
     console.log('Fetching categories')
     for (const localCategoryName of localCategories) {
+        if (localCategoryName.startsWith('.'))
+            continue
+
+        const slug = slugify(localCategoryName)
+
         try {
-            const remoteCategory = await client.getCategory(slugify(localCategoryName))
-            remoteTree.set(remoteCategory.body.slug, { category: remoteCategory.body, docs: [] })
+            const [remoteCategory, remoteDocs] = await Promise.all([
+                client.getCategory(slug),
+                client.getCategoryDocs(slug),
+            ])
+            assert(remoteCategory.body.slug === slug)
+            console.log(`Got ${yellow(localCategoryName)}`)
+            remoteTree.set(slug, {
+                category: remoteCategory.body,
+                docs: remoteDocs.body,
+            })
         } catch (e) {
             if (e instanceof HTTPError) {
                 if (e.response.statusCode == 404) {
-                    console.log(`I cannot create categories yet. Please manually create the category "${localCategoryName}" (slug "${slugify(localCategoryName)}) in Readme.`)
+                    console.log(`I cannot create categories yet. Please manually create the category ${localCategoryName} (slug ${slug}) in Readme.`)
                     errored = true
                 }
             }
@@ -141,14 +163,6 @@ async function main(): Promise<void> {
 
     if (errored)
         return
-
-    for (const [, { category }] of remoteTree) {
-        const remoteDocs = await client.getCategoryDocs(category.slug)
-        remoteTree.set(category.slug, {
-            category,
-            docs: remoteDocs.body,
-        })
-    }
 
     console.log(require('util').inspect(remoteTree, { depth: 999 }))
     await sync(remoteTree)
--- sync/slugify.ts
+++ sync/slugify.ts
@@ -1,3 +1,3 @@
 export function slugify(name: string): string {
-    return name.toLowerCase().replace(/ /g, '-')
+    return name.toLowerCase().replace(/[^A-Za-z0-9-]/g, '-')
 }
