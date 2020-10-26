9021190ff9a6c4e892612d9992c541aa4450d770
Ben Iofel <ben@flow.io>
2020-10-26 02:00:14 -0400

    Keep track of 3rd-level docs that we create while moving pages to delete them later

---
+++ sync/util.test.ts
@@ -0,0 +1,110 @@
+import { findSlugInTree, removeSlugFromTree, addDocUnderSlug } from './util'
+
+test('findSlugInTree', () => {
+    const doc = {
+        slug: 'slug',
+        children: []
+    }
+    expect(findSlugInTree({
+        category: null,
+        docs: [doc]
+    }, 'slug')).toBe(doc)
+
+    expect(findSlugInTree({
+        category: null,
+        docs: [{
+            slug: 'a',
+            children: [{
+                slug: 'b',
+                children: [
+                    {
+                        slug: 'c',
+                        children: []
+                    },
+                    doc
+                ]
+            }]
+        }]
+    }, 'slug')).toBe(doc)
+
+    expect(findSlugInTree({
+        category: null,
+        docs: [{
+            slug: 'a',
+            children: [{
+                slug: 'b',
+                children: [
+                    {
+                        slug: 'c',
+                        children: []
+                    },
+                    {
+                        slug: 'd',
+                        children: [],
+                    }
+                ]
+            }]
+        }]
+    }, 'slug')).toBe(null)
+})
+
+test('removeSlugFromTree', () => {
+    const b = {
+        slug: 'b',
+        children: [],
+    }
+    const a = {
+        slug: 'a',
+        children: [b],
+    }
+    const tree = {
+        category: null,
+        docs: [a],
+    }
+
+    expect(removeSlugFromTree(tree, 'd')).toBeNull()
+
+    expect(removeSlugFromTree(tree, 'b')).toBe(b)
+    expect(a.children).toHaveLength(0)
+
+    expect(removeSlugFromTree(tree, 'a')).toBe(a)
+    expect(tree.docs).toHaveLength(0)
+})
+
+test('addDocUnderSlug', () => {
+    const tree = {
+        category: null,
+        docs: [{
+            slug: 'a',
+            children: [
+                {
+                    slug: 'b',
+                    children: []
+                },
+                {
+                    slug: 'c',
+                    children: []
+                }
+            ]
+        }]
+    }
+
+    const newDoc = {
+        slug: 'd',
+        children: []
+    }
+
+    expect(addDocUnderSlug(tree, newDoc, 'c')).toBeTruthy()
+    expect(tree.docs[0].children[1].children[0]).toBe(newDoc)
+
+    expect(addDocUnderSlug(tree, {
+        slug: 'x',
+        children: []
+    }, 'g')).toBeFalsy()
+
+    expect(addDocUnderSlug(tree, {
+        slug: 'x',
+        children: []
+    }, null)).toBe(true)
+    expect(tree.docs[1].slug).toBe('x')
+})
